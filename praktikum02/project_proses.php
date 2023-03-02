e10fd9e6dc1e46bb6ac3943766afa7b9c8145ce6
Alsyaa <115056364+machaalattee@users.noreply.github.com>
2023-03-02 15:25:06 +0700

    Delete praktikum02 directory

--- praktikum02/project_proses.php
+++
@@ -1,9 +0,0 @@
-<?php
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-if($username == "admin" && $password == "123") {
-    echo "Selamat, kamu berhasil login !";
-} else {
-    echo "Maaf, kamu gagal login, coba lagi !";
-}
