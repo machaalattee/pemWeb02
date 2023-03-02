58fd188cdb4aabb5fe1cc311447455031713a2ba
Alsyaa <115056364+machaalattee@users.noreply.github.com>
2023-03-02 15:26:03 +0700

    Praktikum 2

---
+++ praktikum02/project_proses.php
@@ -0,0 +1,9 @@
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if($username == "admin" && $password == "123") {
+    echo "Selamat, kamu berhasil login !";
+} else {
+    echo "Maaf, kamu gagal login, coba lagi !";
+}
