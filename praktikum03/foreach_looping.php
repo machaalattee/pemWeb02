d5d816d19821207494cbafa32ac6500dd7be4cd0
Alsyaa <115056364+machaalattee@users.noreply.github.com>
2023-03-09 15:08:05 +0700

    Praktikum 3

---
+++ praktikum03/foreach_looping.php
@@ -0,0 +1,6 @@
+<?php
+$arrayHewan = ['Kepiting','Kelinci','Kambing','Kura-kura'];
+
+foreach($arrayHewan as $key => $value) {
+    echo "$key adalah $value <br>";
+}
---
+++ praktikum03/function.php
@@ -0,0 +1,11 @@
+<?php
+function hitungUmur($tahunLahir){
+    $hitungUmur = 2023 - $tahunLahir;
+    echo $hitungUmur;
+}
+
+hitungUmur(2001);
+echo "<br>";
+hitungUmur(2000);
+echo "<br>";
+hitungUmur(1998);
